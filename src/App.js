6f453df359bc20c417c8109f0a0c1cad462a29cd
seawin97 <seawin97@naver.com>
2025-06-06 19:53:11 +0900

    Resolve merge and restore changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MainPage from './components/page/MainPage';
+import DetailPage from './components/page/DetailPage';
+import CartPage from './components/page/CartPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* 라우팅 설정: 페이지 간 이동을 위한 Router */}
+      <BrowserRouter>
+        <Routes>
+          {/* 메인 페이지 (전체 상품 목록) */}
+          <Route path="/" element={<MainPage />} />
+          {/* 상품 상세 페이지 (productId에 따라 동적 경로 처리) */}
+          <Route path="detail/:productId" element={<DetailPage />} />
+          {/* 장바구니 페이지 */}
+          <Route path="cart" element={<CartPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
